880579d7d57a96d0aea6603d99083d160ce1bb73
jounumar <44790212+jounumar@users.noreply.github.com>
2018-11-18 01:50:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Namrod ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$nplay | Namrod Music`,"http://twitch.tv/Namrod")
+client.user.setGame(`$aplay | Script By : Namrod`,"http://twitch.tv/Namrod")
 client.user.setStatus("dnd")
 });
 
